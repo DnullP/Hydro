1dac320ed481a75c76b405ddf19d4996dec21273
Dnull_P <dnull123p@gmail.com>
2025-04-15 14:09:53 +0000

    fix loj

--- loj_download.sh
+++ loj_download.sh
@@ -1,5 +1,5 @@
-node node_modules/@hydrooj/loj-download/bin/loj-download.js https://loj.ac/p/$1
+node loj-download/bin/loj-download.js https://loj.ac/p/$1
 
-zip -r node_modules/@hydrooj/loj-download/downloads/compressed_p/$1 node_modules/@hydrooj/loj-download/downloads/loj.ac/$1
+zip -r loj-download/downloads/compressed_p/$1 loj-download/downloads/loj.ac/$1
 
-node node_modules/hydrooj/bin/hydrooj.js cli problem import system node_modules/@hydrooj/loj-download/downloads/compressed_p/$1.zip
+node node_modules/hydrooj/bin/hydrooj.js cli problem import system loj-download/downloads/compressed_p/$1.zip
